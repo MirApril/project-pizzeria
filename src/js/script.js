dbc642fb122a60b4943659189334d206f73a9b47
Mira Kwiecińska <mira.kwiecinska@gmail.com>
2022-04-23 18:56:58 +0200

    add toggle cart

--- src/js/script.js
+++ src/js/script.js
@@ -5,52 +5,86 @@
 
 	const select = {
 		templateOf: {
-			menuProduct: '#template-menu-product',
+		  menuProduct: '#template-menu-product',
+		  cartProduct: '#template-cart-product', // CODE ADDED
 		},
 		containerOf: {
-			menu: '#product-list',
-			cart: '#cart',
+		  menu: '#product-list',
+		  cart: '#cart',
 		},
 		all: {
-			menuProducts: '#product-list > .product',
-			menuProductsActive: '#product-list > .product.active',
-			formInputs: 'input, select',
+		  menuProducts: '#product-list > .product',
+		  menuProductsActive: '#product-list > .product.active',
+		  formInputs: 'input, select',
 		},
 		menuProduct: {
-			clickable: '.product__header',
-			form: '.product__order',
-			priceElem: '.product__total-price .price',
-			imageWrapper: '.product__images',
-			amountWidget: '.widget-amount',
-			cartButton: '[href="#add-to-cart"]',
+		  clickable: '.product__header',
+		  form: '.product__order',
+		  priceElem: '.product__total-price .price',
+		  imageWrapper: '.product__images',
+		  amountWidget: '.widget-amount',
+		  cartButton: '[href="#add-to-cart"]',
 		},
 		widgets: {
-			amount: {
-				input: 'input[name="amount"]',
-				linkDecrease: 'a[href="#less"]',
-				linkIncrease: 'a[href="#more"]',
-			},
+		  amount: {
+			input: 'input.amount', // CODE CHANGED
+			linkDecrease: 'a[href="#less"]',
+			linkIncrease: 'a[href="#more"]',
+		  },
 		},
-	}
-
-	const classNames = {
+		// CODE ADDED START
+		cart: {
+		  productList: '.cart__order-summary',
+		  toggleTrigger: '.cart__summary',
+		  totalNumber: `.cart__total-number`,
+		  totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+		  subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+		  deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+		  form: '.cart__order',
+		  formSubmit: '.cart__order [type="submit"]',
+		  phone: '[name="phone"]',
+		  address: '[name="address"]',
+		},
+		cartProduct: {
+		  amountWidget: '.widget-amount',
+		  price: '.cart__product-price',
+		  edit: '[href="#edit"]',
+		  remove: '[href="#remove"]',
+		},
+		// CODE ADDED END
+	  };
+	  
+	  const classNames = {
 		menuProduct: {
-			wrapperActive: 'active',
-			imageVisible: 'active',
+		  wrapperActive: 'active',
+		  imageVisible: 'active',
 		},
-	}
-
-	const settings = {
+		// CODE ADDED START
+		cart: {
+		  wrapperActive: 'active',
+		},
+		// CODE ADDED END
+	  };
+	  
+	  const settings = {
 		amountWidget: {
-			defaultValue: 1,
-			defaultMin: 1,
-			defaultMax: 10,
+		  defaultValue: 1,
+		  defaultMin: 1,
+		  defaultMax: 9,
+		}, // CODE CHANGED
+		// CODE ADDED START
+		cart: {
+		  defaultDeliveryFee: 20,
 		},
-	}
-
-	const templates = {
+		// CODE ADDED END
+	  };
+	  
+	  const templates = {
 		menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
-	}
+		// CODE ADDED START
+		cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+		// CODE ADDED END
+	  };
 
 	class Product {
 		constructor(id, data) {
@@ -228,6 +262,37 @@
 
 }
 
+	class Cart{
+		constructor(element){
+			const thisCart = this;
+
+			thisCart.products = [];
+
+			thisCart.getElements(element);
+			thisCart.initActions();
+
+			console.log('new Cart', thisCart)
+		}
+
+		getElements(element){
+			const thisCart = this;
+
+			thisCart.dom = {};
+
+			thisCart.dom.wrapper = element;
+			thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+		}
+		initActions(){
+			const thisCart = this;
+
+			thisCart.dom.toggleTrigger.addEventListener('click', function(event){
+				event.preventDefault();
+			
+				thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+			  });
+		}
+	}
+
 	const app = {
 		initMenu: function () {
 			const thisApp = this
@@ -253,8 +318,16 @@
 			console.log('settings:', settings)
 			console.log('templates:', templates) */
 
-			thisApp.initData()
-			thisApp.initMenu()
+			thisApp.initData();
+			thisApp.initMenu();
+			thisApp.initCart();
+		},
+
+		initCart: function(){
+			const thisApp = this;
+
+			const cartElem = document.querySelector(select.containerOf.cart);
+			thisApp.cart = new Cart(cartElem);
 		},
 	}
 
